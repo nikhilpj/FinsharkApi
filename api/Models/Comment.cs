cc3573c977d426ec1153d5cd14b8936996a420f5
nikhil <nikhilpj98@gmail.com>
2025-10-16 18:31:59 +0530

    created stock and comment models

---
+++ api/Models/Comment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mime;
+
+public class Comment
+{
+	public int Id { get; set; }
+	public string Title { get; set; } = string.Empty;
+	public string Content { get; set; } = string.Empty;
+	public DateTime CreatedOn { get; set; } = DateTime.Now;
+	public int? StockId { get; set; }
+	public Stock? Stock { get; set; }
+}
---
+++ api/Models/Stock.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Stock
+{
+    public int Id { get; set; }
+    public string Symbol { get; set; } = string.Empty;
+    public string CompanyName { get; set; } = string.Empty;
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal Purchase { get; set; }
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal Divident { get; set; }
+    public decimal LastDiv { get; set; }
+    public string Industry { get; set; } = string.Empty;
+    public long MarketCap { get; set; }
+    public List<Comment> Comments { get; set; } = new List<Comment>();
+}
