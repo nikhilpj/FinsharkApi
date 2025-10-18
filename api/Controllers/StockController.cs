ec9a4388345240ff6089a00967eebff75883ad92
nikhil <nikhilpj98@gmail.com>
2025-10-18 14:58:24 +0530

    Created Stock Controller and Getall method to get all the stock data

---
+++ api/Controllers/StockController.cs
@@ -0,0 +1,24 @@
+﻿using api.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+       
+        private readonly ApplicationDbContext _context;
+        public StockController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var stocks = _context.Stock.ToList();
+            return Ok(stocks);
+        }
+    }
+}
--- api/obj/Release/net8.0/api.AssemblyInfo.cs
+++ api/obj/Release/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc3573c977d426ec1153d5cd14b8936996a420f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+122649270f2858287357e450697c922ea5b2e7b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
